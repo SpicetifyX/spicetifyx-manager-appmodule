77a877c526d6b05ff06a850c9babb60cdabe09d6
Jeffrey Reij <contact@jreij.info>
2026-02-23 12:57:53 +0100

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+export default function Footer({ children, hidden }: { children?: ReactNode; hidden?: boolean }) {
+  return (
+    <div className={`flex h-16 w-full shrink-0 items-center justify-end gap-4 ${!hidden && "border-t border-[#2a2a2a] bg-[#121418]"} px-6`}>
+      {children && children}
+    </div>
+  );
+}
