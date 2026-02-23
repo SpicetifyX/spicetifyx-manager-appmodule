b1dc49421611f2a3990ff2b781a2915c3aaea4ba
Jeffrey Reij <contact@jreij.info>
2026-02-23 17:42:03 +0100

    Update ApplyModal.tsx

--- src/components/ApplyModal.tsx
+++ src/components/ApplyModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaCheckCircle } from "react-icons/fa";
 import Spinner from "./Spinner";
 
