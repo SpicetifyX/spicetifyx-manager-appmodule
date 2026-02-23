b3afa38fa0a871f8a298ce615b600aa22c7f8d48
Jeffrey Reij <contact@jreij.info>
2026-02-23 12:57:50 +0100

    Create EmptyState.tsx

---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  label: string;
+}
+
+export default function EmptyState({ label }: Props) {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <span className="text-lg text-[#a0a0a0]">{label}</span>
+    </div>
+  );
+}
